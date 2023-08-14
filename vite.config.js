fadcb27fbeb40229bda76a7ada20394dec981a96
Milenko Castillo <milenko.castillo@gmail.com>
2023-08-13 20:31:46 -0400

    github pages setup

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/portfolio/",
   plugins: [
     vue(),
     VueI18nPlugin({}),
